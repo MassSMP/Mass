c03350e8744f5981efbc45291a5cc0af6e05da81
José Benavente <bgmp00@protonmail.com>
2020-02-11 22:26:50 -0300

    Update restart time to 10 seconds

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 while true
 do
 java -Xms10000M -Xmx10000M -Djline.terminal=jline.UnsupportedTerminal -jar latest.jar
-echo "Server restarting in 5 seconds..."
-sleep 5
+echo "Server restarting in 10 seconds..."
+sleep 10
 done
