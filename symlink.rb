9346f0bf426454511aed838edefa05f147f5caab
BGM <bgmp00@protonmail.com>
2020-02-11 22:05:40 -0300

    Automate symlinking server files

---
+++ symlink.rb
@@ -0,0 +1,9 @@
+REPO_DIR = '/home/Mass/'
+SERVER_DIR = '/home/Mass-Server/'
+FILES = %w(eula.txt server.properties latest.jar server-icon.png start.sh restart.sh)
+
+puts('=== Creating symlinks ===')
+	FILES.each do |file|
+		system('ln -s ' + REPO_DIR + file + ' ' + SERVER_DIR)
+  end
+puts('=== Created symlinks ===')
