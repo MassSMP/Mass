1a7b13ae07cd0e982d973cfea61268d77fc51086
José Benavente <bgmp00@protonmail.com>
2020-02-13 16:52:37 -0300

    Fix default server gui's memory leak

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 while true
 do
-java -Xms10000M -Xmx10000M -Djline.terminal=jline.UnsupportedTerminal -jar latest.jar
+java -Xms10000M -Xmx10000M -Djline.terminal=jline.UnsupportedTerminal -jar latest.jar nogui
 echo "Server restarting in 10 seconds..."
 sleep 10
 done
